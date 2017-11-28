46a41d7ba82fd15e01c7ba4b5d6d215db0008cc9
Callum Hutchinson <ctyh@umich.edu>
2017-11-28 15:39:28 -0500

    visualize changes

--- visualize.py
+++ visualize.py
@@ -1,3 +1,11 @@
+'''
+Format:
+0                    1              2            3                4
+Net Votes,Hours since submitted,Subreddit,Upvote Ratio,Num users on subreddit
+
+Pandas[col][row]
+'''
+
 import pandas as pd
 import matplotlib.pyplot as plt
 data = pd.read_csv("scraped_data.txt",header=None, delimiter=" ")
@@ -5,12 +13,6 @@ data = pd.read_csv("scraped_data.txt",header=None, delimiter=" ")
 timeplot = []
 scoreplot = []
 
-for i in range(0,len(data)):
-	if (data[2][i]=="AskReddit"):
-		scoreplot.append(data[0][i])
-		timeplot.append(data[1][i])
-
-plt.scatter(timeplot, scoreplot)
-plt.show()
+plt.hist(data[2])
 
 
